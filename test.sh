02f2833e7b8b80854d20efe14a694cff6bd48899
Meng Jiang <mjiang89@gmail.com>
2016-02-03 17:29:33 -0600

    clean!

--- test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-rm tweetcube
-g++ -lm -pthread -Ofast -march=native -Wall -funroll-loops -ffast-math -Wno-unused-result tweetcube.cpp -o tweetcube -lgsl -lm -lgslcblas
-./tweetcube -workdir data -input cube -dim 4 -metric 2 -depth 5 -seedsize 3 -seeds 3 -threads 3 -iter 30
